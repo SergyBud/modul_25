41022b22cb6f7e4d30815738862dccb76256f41c
SergyBud <Lestat2007000@gmail.com>
2021-08-18 14:01:46 +0300

    first commit

---
+++ 2.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+import time
+import pytest
+
+@pytest.fixture(autouse=True)
+def testing():
+    pytest.driver = webdriver.Chrome('C:\chromedriver_win32\chromedriver.exe')
+    pytest.driver.get('http://petfriends1.herokuapp.com/login')
+    wait = WebDriverWait(driver, 10)
+    element = wait.until(EC.element_to_be_clickable((By.ID, 'someid')))
+
+    yield
+
+    pytest.driver.quit()
+
+    def test_show_my_pets():
+        pytest.driver.find_element_by_id('email').send_keys('vaya@mail.com')
+        driver.implicitly_wait(5)
+        pytest.driver.find_element_by_id('pass').send_keys('1245')
+        driver.implicitly_wait(5)
+        pytest.driver.find_element_by_css_selector('button[type="submit"]').click()
+        driver.implicitly_wait(5)
+        assert pytest.driver.find_element_by_tag_name('h1').text == "PetFriends"
+
+    images = pytest.driver.find_elements_by_css_selector('.card-deck .card-img-top')
+    names = pytest.driver.find_elements_by_css_selector('.card-deck .card-img-top')
+    descriptions = pytest.driver.find_elements_by_css_selector('.card-deck .card-img-top')
+
+        for i in range(len(names)):
+        assert images[i].get_attribute('src') != ''
+        assert names[i].text != ''
+        assert descriptions[i].text != ''
+        assert ', ' in descriptions[i]
+        parts = descriptions[i].text.split(", ")
+        assert len(parts[0]) > 0
+        assert len(parts[1]) > 0
